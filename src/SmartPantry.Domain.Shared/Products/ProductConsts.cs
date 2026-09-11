eac9166083a644d04ff3a65bb0c1ff5608b8615f
Benjamín Telli <ben6jer9@gmail.com>
2026-09-11 11:00:57 -0300

    domain y domain.shared TP5

---
+++ src/SmartPantry.Domain.Shared/Products/ProductConsts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static class ProductConsts
+{
+    public const int MaxBarcodeLength = 64;
+    public const int MaxNameLength = 128;
+    public const int MaxBrandLength = 128;
+    public const int MaxQuantityLength = 64;
+    public const int MaxIngredientsLength = 1024;
+    public const int MaxAllergensLength = 512;
+}
